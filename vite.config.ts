8d273d97124cef2591f71e5b4eb91e73af88d92b
Vani <vanixjnk@gmail.com>
2026-02-11 16:33:51 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite"
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist/public',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    proxy: {
+       '/api': 'http://localhost:3000',
+    }
+  }
+});
