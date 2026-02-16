7d62c6fd5f8c19857ef88bdbb6e70eb1e0197dc3
Vani <vanixjnk@gmail.com>
2026-02-17 00:07:15 +0700

    feat: add authentication proxy for global authentication state and a new configuration page component

--- src/proxies/authentication.proxy.ts
+++ src/proxies/authentication.proxy.ts
@@ -1,8 +1,6 @@
 import { Elysia } from "elysia";
 import { verifyToken } from "@/controllers/authentication/authentication.controller";
-
 const COOKIE_NAME = "vani_auth";
-
 export const authProxy = new Elysia({ name: "auth-proxy" })
   .derive({ as: "global" }, ({ cookie }) => {
     const token = cookie[COOKIE_NAME]?.value as string | undefined;
