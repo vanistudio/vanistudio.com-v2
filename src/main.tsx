893533ad1ab8a8d39171c8f44cad22b36b5f60df
Vani <vanixjnk@gmail.com>
2026-02-17 15:27:47 +0700

    feat: Set up initial global styling, theming, and core React providers including a Sileo toaster.

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,32 @@ import './index.css';
 import { ProgressProviders } from './components/providers/ProgressProvider';
 import { ConsoleProvider } from './components/providers/ConsoleProvider';
 import { ThemeProvider } from './components/providers/ThemeProvider';
+import { useTheme } from 'next-themes';
 import { Toaster } from "sileo";
 import "sileo/styles.css";
 
+function SileoToaster() {
+  const { resolvedTheme } = useTheme();
+  const isDark = resolvedTheme === "dark";
+  return (
+    <Toaster
+      position="top-center"
+      options={{
+        fill: isDark ? "black" : "white",
+        styles: { description: isDark ? "text-white/75!" : "text-black/75!" },
+      }}
+    />
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Toaster position="top-center" />
     <ProgressProviders>
       <ConsoleProvider />
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <SileoToaster />
         <App />
       </ThemeProvider>
     </ProgressProviders>
   </React.StrictMode>
 );
-
