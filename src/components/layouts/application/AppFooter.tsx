21818ee375c268785a0af10c83c82a0a2c4b250b
Vani <vanixjnk@gmail.com>
2026-02-19 19:01:25 +0700

    feat: implement core application layout components including header, footer, and main layout structure

--- src/components/layouts/application/AppFooter.tsx
+++ src/components/layouts/application/AppFooter.tsx
@@ -1,6 +1,6 @@
-import { SiGithub, SiX, SiDiscord } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 import AppDashed from './AppDashed';
+
 const AppFooter = () => {
   const currentYear = new Date().getFullYear();
 
@@ -15,24 +15,6 @@ const AppFooter = () => {
 
   return (
     <footer className="w-full mt-auto">
-      <AppDashed noTopBorder padding="p-8 pb-12">
-        <div className="flex flex-col gap-2 justify-center items-center">
-          <div className="flex items-center gap-2">
-            <img src='/vanistudio.png' className='h-[80px]' />
-          </div>
-          <p className="text-sm text-muted-foreground max-w-[480px] text-center">
-            Thiết kế và phát triển Website để giúp cho việc kiếm tiền online của bạn trở nên tự động hóa và hiệu quả hơn. 
-          </p>
-          <div className="flex gap-3">
-            <a href="//github.com/vanistudio" target='_blank' rel="noreferrer" className="p-2 border border-border rounded-md hover:bg-muted-background transition-colors text-muted-foreground hover:text-foreground text-primary">
-              <SiGithub size={16} />
-            </a>
-            <a href="//x.com/primary" target='_blank' rel="noreferrer" className="p-2 border border-border rounded-md hover:bg-muted-background transition-colors text-muted-foreground hover:text-foreground text-primary">
-              <SiX size={16} />
-            </a>
-          </div>
-        </div>
-      </AppDashed>
       <AppDashed noTopBorder padding="p-4">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="flex items-center gap-2">
@@ -58,19 +40,6 @@ const AppFooter = () => {
           </div>
         </div>
       </AppDashed>
-      <div className="h-full">
-        <div
-          className="max-w-5xl mx-5 md:mx-auto relative p-3 h-full"
-          style={{
-            backgroundImage:
-              "repeating-linear-gradient(to bottom, var(--border-color) 0px, var(--border-color) 6px, transparent 6px, transparent 14px),          repeating-linear-gradient(to bottom, var(--border-color) 0px, var(--border-color) 6px, transparent 6px, transparent 14px)",
-            backgroundPosition: "left top, right top",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "1px 100%, 1px 100%",
-          }}>
-          <div className="w-full h-full sm:min-h-[220px] min-h-[100px] bg-dot-grid" />
-        </div>
-      </div>
     </footer>
   );
 };
